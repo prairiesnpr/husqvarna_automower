691d81de43fe61deffac9c6a21c951756dd2ce06
PrairieSnpr <midnryanmiller@gmail.com>
2023-05-20 16:34:41 -0400

    Format with black

--- custom_components/husqvarna_automower/sensor.py
+++ custom_components/husqvarna_automower/sensor.py
@@ -347,6 +347,7 @@ class AutomowerZoneSensor(SensorEntity, AutomowerEntity):
         """Return the specific state attributes of this mower."""
         return {ZONE_ID: self.zone_id}
 
+
 class AutomowerSensor(SensorEntity, AutomowerEntity):
     """Defining the Automower Sensors with AutomowerSensorEntityDescription."""
 
